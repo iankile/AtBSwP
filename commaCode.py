a8ddefe2e62c241c4cc1a1d6d977604682a1269a
iankile <iankile@gmail.com>
2016-10-05 19:02:28 -0600

    Finished commaCode!
    used end parameter that I don’t fully understand

--- commaCode.py
+++ commaCode.py
@@ -3,6 +3,6 @@ info = None
 while info != '':
     info = str(input('What do you want to add to your list? '))
     list.append(str(info))
-for i in range(int(len(list)-1)):
-    print(list[i] + ', ' )
-print(list[:-1])
+for i in range(int(len(list)-2)):
+    print(list[i] + ', ', end='')
+print('and ' + list[-2])
