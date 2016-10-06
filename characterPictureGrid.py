bd45009b31a42999afe3d39942507ec3d9d74105
iankile <iankile@gmail.com>
2016-10-05 21:12:39 -0600

    Added encouragement

--- characterPictureGrid.py
+++ characterPictureGrid.py
@@ -18,11 +18,4 @@ while x != 9:
         y += 1
     x += 1
 
-# b = 0
-# while b <= 10:
-#     x = 0
-#     print(b)
-#     while x <= 15:
-#         print(x, end='')
-#         x += 1
-#     b += 1
+# try nested 'for' loops noob!
